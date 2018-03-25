647895d3dfe8378ce0e2a29cb09bd41a9bab0b81
Adrian Rabowski <adrian.rabowski@gmail.com>
2018-03-25 14:17:36 +0200

    fix to exercise 4

--- Book_Catalogue/src/main/java/pl/rabowski/Book_Catalogue/BookCatalogueApplication.java
+++ Book_Catalogue/src/main/java/pl/rabowski/Book_Catalogue/BookCatalogueApplication.java
@@ -1,9 +1,11 @@
 package pl.rabowski.Book_Catalogue;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration() //exclude={DataSourceAutoConfiguration.class}
 public class BookCatalogueApplication {
 
 	public static void main(String[] args) {
--- Book_Catalogue/src/main/java/pl/rabowski/Book_Catalogue/controllers/BookController.java
+++ Book_Catalogue/src/main/java/pl/rabowski/Book_Catalogue/controllers/BookController.java
@@ -1,12 +1,13 @@
 package pl.rabowski.Book_Catalogue.controllers;
 
 
-
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.rabowski.Book_Catalogue.models.Book;
 
+@EnableAutoConfiguration
 @RestController
 @RequestMapping("/books")
 public class BookController	{
