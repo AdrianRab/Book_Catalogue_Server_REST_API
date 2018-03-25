1c1d84eeb0cd87c5e990113bb6dc2312d2354911
Adrian Rabowski <adrian.rabowski@gmail.com>
2018-03-25 15:32:58 +0200

    Zadanie 8

--- Book_Catalogue/src/main/java/pl/rabowski/Book_Catalogue/controllers/BookController.java
+++ Book_Catalogue/src/main/java/pl/rabowski/Book_Catalogue/controllers/BookController.java
@@ -1,12 +1,13 @@
 package pl.rabowski.Book_Catalogue.controllers;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,35 +18,46 @@ import pl.rabowski.Book_Catalogue.models.MemoryBookService;
 @EnableAutoConfiguration
 @RestController
 @RequestMapping("/books")
-public class BookController	{
+public class BookController {
 	@RequestMapping("/hello")
-	public	String	hello(){
+	public String hello() {
 		return "{hello: World}";
 	}
 
 	@RequestMapping("/helloBook")
-	public	Book helloBook(){
-		return new Book(1,"9788324631766","Thinking	in	Java",
-			"Bruce	Eckel","Helion","programming");
+	public Book helloBook() {
+		return new Book(1, "9788324631766", "Thinking	in	Java", "Bruce	Eckel", "Helion", "programming");
 	}
-	
+
 	@Autowired
 	private MemoryBookService memoryBookService;
-	
+
 	@RequestMapping("/allBooks")
-	public List<Book> listOfAllBooks(){
+	public List<Book> listOfAllBooks() {
 		return memoryBookService.getList();
 	}
-	
+
 	@RequestMapping("/book/{id}")
-	public Book listOfAllBooks(@PathVariable int id){
+	public Book listOfAllBooks(@PathVariable int id) {
 		return memoryBookService.getBook(id);
 	}
-	
-	@PostMapping(path="/addBook")
-	public Book addBook(@RequestParam  int id, @RequestParam  String  isbn, @RequestParam  String  title, 
-			@RequestParam  String  author, @RequestParam  String  publisher, @RequestParam  String  type){
-	return memoryBookService.addBook(id, isbn, title, author, publisher, type);
+
+	@PostMapping(path = "/addBook")
+	public Book addBook(@RequestParam int id, @RequestParam String isbn, @RequestParam String title,
+			@RequestParam String author, @RequestParam String publisher, @RequestParam String type) {
+		return memoryBookService.addBook(id, isbn, title, author, publisher, type);
+	}
+
+	@PutMapping(path = "/editBook/{id}")
+	public Book editBook(@PathVariable int id, @RequestParam String isbn, @RequestParam String title,
+			@RequestParam String author, @RequestParam String publisher, @RequestParam String type) {
+		return memoryBookService.editBook(id, isbn, title, author, publisher, type);
 	}
 	
+	@DeleteMapping(path="deleteBook/{id}")
+	public String deleteBook(@PathVariable int id) {
+		memoryBookService.deleteBook(id);
+		return "Book has been deleted";
+	}
+
 }
--- Book_Catalogue/src/main/java/pl/rabowski/Book_Catalogue/models/MemoryBookService.java
+++ Book_Catalogue/src/main/java/pl/rabowski/Book_Catalogue/models/MemoryBookService.java
@@ -12,8 +12,10 @@ public class MemoryBookService {
 	public MemoryBookService() {
 		list = new ArrayList<>();
 		list.add(new Book(1, "9788324631766", "Thinking in Java", "Bruce Eckel", "Helion", "programming"));
-		list.add(new Book(2, "9788324627738", "Rusz glowa, Java.", "Sierra Kathy, Bates	Bert", "Helion", "programming"));
-		list.add(new Book(3, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary Cornell", "Helion", "programming"));
+		list.add(new Book(2, "9788324627738", "Rusz glowa, Java.", "Sierra Kathy, Bates	Bert", "Helion",
+				"programming"));
+		list.add(new Book(3, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary Cornell", "Helion",
+				"programming"));
 	}
 
 	public List<Book> getList() {
@@ -23,19 +25,33 @@ public class MemoryBookService {
 	public void setList(List<Book> list) {
 		this.list = list;
 	}
-	
+
 	public Book getBook(int bookIndex) {
-		for(int i = 0; i<list.size(); i ++) {
-			if(list.get(i).getId() == bookIndex) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getId() == bookIndex) {
 				return list.get(i);
 			}
 		}
 		return null;
 	}
-	
+
 	public Book addBook(int id, String isbn, String title, String author, String publisher, String type) {
 		Book book = new Book(id, isbn, title, author, publisher, type);
 		list.add(book);
 		return book;
+	}
+
+	public Book editBook(int id, String isbn, String title, String author, String publisher, String type) {
+		Book editedBook = this.getBook(id);
+		editedBook.setIsbn(isbn);
+		editedBook.setTitle(title);
+		editedBook.setAuthor(author);
+		editedBook.setPublisher(publisher);
+		editedBook.setType(type);
+		return editedBook;
+	}
+
+	public void deleteBook(int id) {
+		list.remove(this.getBook(id));
 	}
 }
