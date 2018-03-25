c7a0d61976c3ef4070bdf6e0fe35ee43008e3e5f
Adrian Rabowski <adrian.rabowski@gmail.com>
2018-03-25 12:30:38 +0200

    Zadanie 1 i 2

---
+++ Book_Catalogue/src/test/java/pl/rabowski/Book_Catalogue/BookCatalogueApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.rabowski.Book_Catalogue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookCatalogueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
