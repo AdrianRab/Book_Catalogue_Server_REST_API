dccbce2f1c6de5e7e006be0e9d5df4523f505c4c
Adrian Rabowski <adrian.rabowski@gmail.com>
2018-03-25 12:46:00 +0200

    Zadanie 3 i 4

---
+++ Book_Catalogue/src/main/java/pl/rabowski/Book_Catalogue/controllers/BookController.java
@@ -0,0 +1,23 @@
+package pl.rabowski.Book_Catalogue.controllers;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pl.rabowski.Book_Catalogue.models.Book;
+
+@RestController
+@RequestMapping("/books")
+public class BookController	{
+	@RequestMapping("/hello")
+	public	String	hello(){
+		return "{hello: World}";
+	}
+
+	@RequestMapping("/helloBook")
+	public	Book helloBook(){
+		return new Book(1,"9788324631766","Thinking	in	Java",
+			"Bruce	Eckel","Helion","programming");
+	}
+}
